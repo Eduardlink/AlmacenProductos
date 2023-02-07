b6d94c25156fd173cc62197f392198b7036e25ea
CCAljo <93953404+CCAljo@users.noreply.github.com>
2023-02-06 22:12:40 -0500

    Login 2.0

--- index1.php
+++ index1.php
@@ -1,5 +1,5 @@
 <?php
-    include_once 'controllers/controller.php';
+    include_once 'controller/controller.php';
     include_once 'includes/model.php';
     $mvc = new MvcController();
     $mvc->plantilla();
