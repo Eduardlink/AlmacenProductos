df950e065343defe8fb597eba298f500334e5213
CCAljo <93953404+CCAljo@users.noreply.github.com>
2023-02-06 22:05:52 -0500

    Mejora CrudBodega

--- test.php
+++ test.php
@@ -51,15 +51,32 @@
                 <div style="margin-bottom:10px">
                     <input name="direccion_bod" class="easyui-textbox" required="true" label="DIRECCION:" style="width:100%">
                 </div>
-                <div style="margin-bottom:10px">
-                    <input name="estado" class="easyui-textbox" required="true" label="ESTADO:" style="width:100%">
-                </div>
             </form>
         </div>
         <div id="dlg-buttons">
             <a href="javascript:void(0)" class="easyui-linkbutton c6" iconCls="icon-ok" onclick="saveUser()" style="width:90px">Guardar</a>
             <a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-cancel" onclick="javascript:$('#dlg').dialog('close')" style="width:90px">Cancelar</a>
         </div>
+
+        <div id="dlgedit" class="easyui-dialog" style="width:400px" data-options="closed:true,modal:true,border:'thin',buttons:'#dlgedit-buttons'">
+            <form id="fmedit" method="post" novalidate style="margin:0;padding:20px 50px">
+                <h3>Informacion de la Bodega</h3>
+                <div style="margin-bottom:10px">
+                    <input name="id_bod" class="easyui-textbox" required="true" label="ID:" style="width:100%" readonly="readonly">
+                </div>
+                <div style="margin-bottom:10px">
+                    <input name="nombre_bod" class="easyui-textbox" required="true" label="NOMBRE:" style="width:100%">
+                </div>
+                <div style="margin-bottom:10px">
+                    <input name="direccion_bod" class="easyui-textbox" required="true" label="DIRECCION:" style="width:100%">
+                </div>
+            </form>
+        </div>
+        <div id="dlgedit-buttons">
+            <a href="javascript:void(0)" class="easyui-linkbutton c6" iconCls="icon-ok" onclick="saveUserEdit()" style="width:90px">Guardar</a>
+            <a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-cancel" onclick="javascript:$('#dlgedit').dialog('close')" style="width:90px">Cancelar</a>
+        </div>
+
         <script type="text/javascript">
             var url;
 
@@ -72,8 +89,8 @@
             function editUser() {
                 var row = $('#dg').datagrid('getSelected');
                 if (row) {
-                    $('#dlg').dialog('open').dialog('center').dialog('setTitle', 'Editar Estudiante');
-                    $('#fm').form('load', row);
+                    $('#dlgedit').dialog('open').dialog('center').dialog('setTitle', 'Editar Estudiante');
+                    $('#fmedit').form('load', row);
                     url = 'model/editarBodega.php?id=' + row.id;
                 }
             }
@@ -94,6 +111,28 @@
                             });
                         } else {
                             $('#dlg').dialog('close'); // close the dialog
+                            $('#dg').datagrid('reload'); // reload the user data
+                        }
+                    }
+                });
+            }
+
+            function saveUserEdit() {
+                $('#fmedit').form('submit', {
+                    url: url,
+                    iframe: false,
+                    onSubmit: function() {
+                        return $(this).form('validate');
+                    },
+                    success: function(result) {
+                        var result = eval('(' + result + ')');
+                        if (result.errorMsg) {                            
+                            $.messager.show({
+                                title: 'Error',
+                                msg: result.errorMsg
+                            });
+                        } else {
+                            $('#dlgedit').dialog('close'); // close the dialog
                             $('#dg').datagrid('reload'); // reload the user data
                         }
                     }
